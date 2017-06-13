acded9755d7f470eb18f3ac4005b7daa3fa16ecd
moses <woesites@gmail.com>
2017-06-13 14:59:19 +0300

    initial stages of car class code

---
+++ car_class.py
@@ -0,0 +1,6 @@
+class Car(object):
+	def __init__(self,name='General', model='GM', vehicle_type='None'):
+		self.name = name
+		self.speed = 0
+		self.model = model
+		self.vehicle_type = vehicle_type
